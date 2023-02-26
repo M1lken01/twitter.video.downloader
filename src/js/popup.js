6a768e31985c4fcfc4e87ac42c1d6fa0b83e73ac
M1lken01 <69786783+M1lken01@users.noreply.github.com>
2023-02-26 14:53:18 +0100

    first update
    added popup, new icon

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,28 @@
-const storageItems = ['downloader', 'errorChecks', 'errorDelay', ];
-const storageItemDefaults = ['savetweetvid', 25, 1000, ];
+const storageItems = ['downloader', 'errorChecks', 'errorDelay', 'currentUrl', 'links'];
+const storageItemDefaults = ['savetweetvid', 25, 1000, '', JSON.stringify([])];
+
+document.addEventListener('DOMContentLoaded', function () {
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function (tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {
+            action: "getURL"
+        }, function (response) {
+            fetchUrl(response);
+        });
+    });
+});
+
+function fetchUrl(response) {
+    setLocalStorage(storageItems[3], response.url);
+    setLocalStorage(storageItems[4], response.links);
+    let links = JSON.parse(localStorage.getItem(storageItems[4]));
+    console.log(links);
+    for (let i = 0; i < links.length; i++) {
+        document.querySelector('#links').innerHTML += '<li><a href="' + links[i] + '">Download ' + links[i].split('vid/')[1].split('/')[0] + '</a></li>';
+    }
+}
 
 function setLocalStorageWithId(item, id) {
     localStorage.setItem(item, document.getElementById(id).value);
